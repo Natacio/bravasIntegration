dd7fcd1fb2bb931d5a56ea5d5b4958a9d0c316ff
Natacio <natacio07@gmail.com>
2022-08-25 08:42:13 -0300

    first commit

---
+++ src/main/java/com/condomob/bravasIntegretion/BravasIntegretionApplication.java
@@ -0,0 +1,13 @@
+package com.condomob.bravasIntegretion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BravasIntegretionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BravasIntegretionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/condomob/bravasIntegretion/BravasIntegretionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condomob.bravasIntegretion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BravasIntegretionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
